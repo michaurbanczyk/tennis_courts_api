fae3f0c81175e817e9c4b6f58f4f1b21e411ada3
Michal Urbanczyk <michaurbanczyk@gmail.com>
2024-12-27 20:09:57 +0100

    run lambda endpoints

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
 import os
 
 LAMBDA_RUN_ENDPOINT = os.getenv("LAMBDA_RUN_ENDPOINT", "")
+LAMBDA_RUN_INTERVAL = os.getenv("LAMBDA_RUN_INVERVAL", "2")
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
--- app/routes/run_lambda.py
+++ app/routes/run_lambda.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.models.run_lambda import RunLambda
+from app.models.run_lambda import RunLambda, RunLambdaStatus, RunLambdaStatusUpdate
 from app.services.run_lambda import RunLambdaService
 
 run_lambda_router = APIRouter(
@@ -14,5 +14,23 @@ def run_lambda():
 
     run_lambda_service = RunLambdaService()
     run_status = run_lambda_service.run_lambda()
+
+    return run_status
+
+
+@run_lambda_router.get("/status", response_model=RunLambdaStatus)
+def get_run_lambda_status():
+
+    run_lambda_service = RunLambdaService()
+    run_status = run_lambda_service.get()
+
+    return run_status
+
+
+@run_lambda_router.patch("/update-status", response_model=RunLambdaStatus)
+def update_run_lambda_status(body: RunLambdaStatusUpdate):
+
+    run_lambda_service = RunLambdaService()
+    run_status = run_lambda_service.update_run_lambda_status(**dict(body))
 
     return run_status
