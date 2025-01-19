f95e624c08da59c812851a3d3de40bc325fde82f
Michal Urbanczyk <michaurbanczyk@gmail.com>
2025-01-19 14:13:40 +0100

    event loop fix

--- app/db/config.py
+++ app/db/config.py
@@ -1,5 +1,7 @@
+from contextlib import asynccontextmanager
 from urllib.parse import quote_plus
 
+from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 
 username = quote_plus("db_admin")
@@ -8,8 +10,23 @@ cluster = "cluster0.k2trl.mongodb.net"
 
 uri = "mongodb+srv://" + username + ":" + password + "@" + cluster
 
-client = AsyncIOMotorClient(uri)
-db = client["tennis"]
-matches_collection = db["matches"]
+client = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup: Initialize MongoDB client
+    global client
+    client = AsyncIOMotorClient(uri)
+    app.state.mongodb = client["tennis"]
+    print("MongoDB connected")
+
+    # Yield control to the app
+    yield
+
+    # Shutdown: Close MongoDB client
+    client.close()
+    print("MongoDB connection closed")
+
 
 connected_clients = []
