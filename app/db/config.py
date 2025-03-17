109cbef07b4cd642700a55ebd2fe62a4bd09d039
Michal Urbanczyk <michaurbanczyk@gmail.com>
2025-03-17 17:09:09 +0100

    is_prod config added

--- app/db/config.py
+++ app/db/config.py
@@ -1,12 +1,18 @@
+import os
 from urllib.parse import quote_plus
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
-username = quote_plus("db_admin")
-password = quote_plus("db_admin")
-cluster = "cluster0.k2trl.mongodb.net"
+username = quote_plus(os.environ.get("DB_USERNAME"))
+password = quote_plus(os.environ.get("DB_PASSWORD"))
+cluster = os.environ.get("MONGO_DB_CLUSTER")
 
 uri = "mongodb+srv://" + username + ":" + password + "@" + cluster
-
 client = AsyncIOMotorClient(uri)
-db = client["tennis"]
+
+IS_PROD = os.environ.get("IS_PROD", False)
+
+if IS_PROD:
+    db = client["tennis"]
+else:
+    db = client["dev"]["tennis"]
