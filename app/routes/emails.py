93a225e0487d9ffff99ab9d4bbc8d65495a7b6e2
Michal Urbanczyk <michaurbanczyk@gmail.com>
2025-05-14 13:10:33 +0200

    changing sending email to fastapi email

--- app/routes/emails.py
+++ app/routes/emails.py
@@ -1,7 +1,8 @@
 import os
+from pathlib import Path
 
 from fastapi import APIRouter, Depends, HTTPException
-from sendgrid import Mail, SendGridAPIClient
+from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
 
 from app.models.common import Response
 from app.models.emails import EmailShareTheLink
@@ -12,26 +13,41 @@ emails_router = APIRouter(
     tags=["emails"],
 )
 
-EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS", "")
-SENDGRID_ACCESS_KEY = os.environ.get("SENDGRID_ACCESS_KEY", "")
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.environ.get("EMAIL_USERNAME"),
+    MAIL_PASSWORD=os.environ.get("EMAIL_PASSWORD"),
+    MAIL_FROM=os.environ.get("EMAIL_USERNAME"),
+    MAIL_PORT=os.environ.get("EMAIL_PORT"),
+    MAIL_SERVER=os.environ.get("EMAIL_SERVER"),
+    MAIL_FROM_NAME="App Open",
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    TEMPLATE_FOLDER=str(Path(__file__).resolve().parent.parent / "templates" / "email"),
+)
+
+
+async def send_email_async(subject: str, email_to: list[str], template_context: dict):
+    message = MessageSchema(
+        subject=subject, recipients=email_to, subtype=MessageType.html, template_body=template_context
+    )
+
+    fm = FastMail(conf)
+    await fm.send_message(message, template_name="email.html")
 
 
 @emails_router.post("/share-tournament-link", response_model=Response)
 async def send_email(body: EmailShareTheLink, current_user: dict = Depends(get_current_user)):
-    body = body.model_dump()
-    if not body['addressList'] or not body["tournamentLink"]:
+    if not body.addressList or not body.tournamentLink:
         raise HTTPException(status_code=400, detail="Empty email address list or transmission link")
 
-    message = Mail(
-        from_email=EMAIL_ADDRESS,
-        to_emails=body['addressList'],
-        subject=f"Join the {body['tournamentTitle']} tournament in App Open",
-        html_content=f'<strong>{body['tournamentLink']}</strong>')
     try:
-        sg = SendGridAPIClient(SENDGRID_ACCESS_KEY)
-        response = sg.send(message)
-        if response.status_code != 202:
-            raise HTTPException(status_code=500, detail="Error with sending emails")
-        return {"message": "Emails have been sent successfully!"}
+        await send_email_async(
+            subject=f"Welcome in {body.tournamentTitle} powered by App Open",
+            email_to=body.addressList,
+            template_context={"body": {"title": "Hello World", "name": "John Doe"}},
+        )
     except Exception:
-        raise HTTPException(status_code=500, detail="Error with sending emails")
+        raise HTTPException(status_code=500, detail="Email sending has been failed.")
+
+    return {"message": "Email has been sent successfully!"}
